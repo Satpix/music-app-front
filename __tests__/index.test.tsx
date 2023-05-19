b2891d7b6412a3afab2158ba9a0a99f027a84d63
Kamash Akhmed <akhmed.kamash@effective-soft.com>
2023-05-19 03:24:06 +0300

    add tests

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import Index from "pages/index";
+import "@testing-library/jest-dom";
+import { describe, expect, test } from "@jest/globals";
+
+  test('renders', () => {
+    render(<Index />);
+    const mainPageElem = screen.getByText(/Main page/i)
+    expect(mainPageElem).toBeInTheDocument();
+  const { container } = render(<Index />)
+  expect(container).toMatchSnapshot()
+  });
+
+  it('renders homepage unchanged', () => {
+    const { container } = render(<Index />)
+    expect(container).toMatchSnapshot()
+  })
